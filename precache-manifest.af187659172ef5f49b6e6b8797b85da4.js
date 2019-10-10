c71356bda134c44ee7faf60ae69fcf8311e9a2e2
Anggeumi <jia5642@naver.com>
2019-10-10 19:47:36 +0900

    Updates

---
+++ precache-manifest.af187659172ef5f49b6e6b8797b85da4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64bb8d6a53455464f35c2b3219992689",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "05378d2715db29591a03",
+    "url": "/movie_app/static/css/main.dfe5042f.chunk.css"
+  },
+  {
+    "revision": "ca6d0ab03409f96801db",
+    "url": "/movie_app/static/js/2.5c162f41.chunk.js"
+  },
+  {
+    "revision": "05378d2715db29591a03",
+    "url": "/movie_app/static/js/main.a0b156e8.chunk.js"
+  },
+  {
+    "revision": "8fbd9badc7454a983c4d",
+    "url": "/movie_app/static/js/runtime~main.9f434370.js"
+  }
+]);
